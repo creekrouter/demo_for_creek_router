34715b22022fbb496e2f22821b955e90116f5b03
lvxiaofei <creekrouter@163.com>
2023-05-03 08:50:40 +0800

    code

--- app/src/main/java/com/hi/mydemo/tools/download/Task.java
+++ app/src/main/java/com/hi/mydemo/tools/download/Task.java
@@ -1,17 +1,13 @@
 package com.hi.mydemo.tools.download;
 
-import com.hi.mydemo.tools.file.FileTool;
 import com.hi.mydemo.tools.file.ZipArchiver;
 import com.hi.mydemo.tools.load.PluginApk;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
 
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 
 public class Task implements StatusListener {
     public enum TaskType {
@@ -31,42 +27,62 @@ public class Task implements StatusListener {
 
     public void startRun() {
         if (this.type == TaskType.DownLoad) {
-            download();
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    download();
+                }
+            }).start();
         } else if (this.type == TaskType.UnZip) {
             unzip();
         }
     }
 
     private void download() {
-        OkHttpClient client = new OkHttpClient.Builder().build();
-        Request.Builder builder = new Request.Builder()
-                .get()
-                .url(apk.httpUrl())
-                .addHeader("Accept-Encoding", "identity")
-                .addHeader("User-Agent", "okhttp/3.10.0[AWV/v1.0;AD/Android]");
-        final Request request = builder.build();
-
-        Call call = client.newCall(request);
-        onStart(this);
-
-        call.enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                onFail(Task.this, e.getMessage());
+        boolean writeComplete = false;
+        listener.onStart(Task.this);
+        try {
+            URL myURL = new URL(apk.httpUrl());
+            URLConnection conn = myURL.openConnection();
+            conn.connect();
+            int contentLength = conn.getContentLength();
+            InputStream is = conn.getInputStream();
+            if (is == null) {
+                listener.onFail(Task.this, "stream is null");
+                return;
             }
 
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                if (response.isSuccessful()) {
-                    File file = new File(apk.pluginPath());
-                    FileTool.writeFile(file, response.body().byteStream(), response.body().contentLength(), Task.this);
-                } else {
-                    onFail(Task.this, "后台response code 错误");
+            //把数据存入路径+文件名
+            FileOutputStream fos = new FileOutputStream(apk.pluginPath());
+            byte buf[] = new byte[1024];
+            int downLoadFileSize = 0;
+            do {
+                //循环读取
+                int numread = is.read(buf);
+                if (numread == -1) {
+                    break;
                 }
+                fos.write(buf, 0, numread);
+                downLoadFileSize += numread;
 
-            }
-        });
+                //计算当前下载进度
+                int progress = (int) (100 * downLoadFileSize / contentLength);
+                listener.onProgress(progress, downLoadFileSize, contentLength);
+                //更新进度条
+            } while (true);
+            is.close();
+            writeComplete = true;
+        } catch (Exception ex) {
+            writeComplete = false;
+            listener.onFail(Task.this, ex.getMessage());
+        }
 
+        if (writeComplete) {
+            //下载完成，并返回保存的文件路径
+            listener.onFinish(Task.this, apk.pluginPath());
+        } else {
+            listener.onFail(Task.this, "download error！");
+        }
     }
 
     private void unzip() {
